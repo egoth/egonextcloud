6d30a5611110f0958cd89a2c04168ac3d055d71d
Ennio Balocchi <ennio@domoticachepassione.it>
2025-08-27 17:11:20 +0200

    utils

--- docker-util-script/crea_admin.sh
+++ docker-util-script/crea_admin.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-sudo docker exec -u www-data -e OC_PASS=Ego.tello1969 -it nextcloud \
+
+# Nome del container
+CONTAINER_NAME="egonextcloud-nextcloud-1"
+
+sudo docker exec -u www-data -e OC_PASS=Ego.tello1969 -it $CONTAINER_NAME \
   php occ user:add --password-from-env --group admin admin
 
