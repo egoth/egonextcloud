f6c2a4eed71d525b3a576f3d61dd3bba9fdd57cd
Ennio Balocchi <ennio@domoticachepassione.it>
2025-08-27 22:00:38 +0200

    utils

--- docker-util-script/crea_users.sh
+++ docker-util-script/crea_users.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-sudo docker exec -u www-data -e OC_PASS=Gyps0l0ne1 -it nextcloud \
+
+# Nome del container
+CONTAINER_NAME="egonextcloud-nextcloud-1"
+
+sudo docker exec -u www-data -e OC_PASS=Gyps0l0ne1 -it $CONTAINER_NAME \
   php occ user:add --password-from-env --group users consuelo
-sudo docker exec -u www-data -e OC_PASS=Gyps0l0ne1 -it nextcloud \
+sudo docker exec -u www-data -e OC_PASS=Gyps0l0ne1 -it $CONTAINER_NAME \
   php occ user:add --password-from-env --group users lucrezia
-sudo docker exec -u www-data -e OC_PASS=Gyps0l0ne1 -it nextcloud \
+sudo docker exec -u www-data -e OC_PASS=Gyps0l0ne1 -it $CONTAINER_NAME \
   php occ user:add --password-from-env --group users leonardo
