19eb5de32ce16063859403cd58d5b81bd3da4737
Ennio Balocchi <ennio@domoticachepassione.it>
2025-08-29 15:48:23 +0200

    Normalizzati i lf

--- apps/egonextapp/appinfo/app.php
+++ apps/egonextapp/appinfo/app.php
@@ -1,11 +1,25 @@
 <?php
-/**
- * Carica lo script solo nell'app Files
- */
-use OCP\Util;
+declare(strict_types=1);
 
-\OCP\Util::connectHook('OCA\Files::loadAdditionalScripts', function () {
-    Util::addScript('egonextapp', 'fileactions'); // carica apps/egonextapp/js/fileactions.js
-    // Se vuoi anche uno stile:
-    // Util::addStyle('egonextapp', 'fileactions');
-});
+namespace OCA\EgoNextApp\AppInfo;
+
+use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCP\AppFramework\Bootstrap\IBootContext;
+
+class Application extends App implements IBootstrap {
+    public const APP_ID = "egonextapp";
+
+    public function __construct() {
+        parent::__construct(self::APP_ID);
+    }
+
+    public function register(IRegistrationContext $context): void {
+        // Registrazioni di servizi, middleware, eventi... (vuoto per ora)
+    }
+
+    public function boot(IBootContext $context): void {
+        // Avvio app (vuoto per ora)
+    }
+}
