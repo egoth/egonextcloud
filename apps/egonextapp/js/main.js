7f9a76df9a66347849e5694fac9b267818d71856
Ennio Balocchi <ennio@domoticachepassione.it>
2025-09-09 07:24:08 +0200

    menu contestuale

--- apps/egonextapp/js/main.js
+++ apps/egonextapp/js/main.js
@@ -1,16 +1,18 @@
 console.log('[egonextapp] main.js caricato');
 
+// Registriamo il plugin per la FileList
 OC.Plugins.register('OCA.Files.FileList', {
     attach: function (fileList) {
-        console.log('[egonextapp] attach su FileList');
+        console.log('[egonextapp] attach eseguito');
 
-        OCA.Files.FileActions.registerAction({
+        fileList.fileActions.registerAction({
             id: 'egonextapp-action',
             displayName: t('egonextapp', 'Mostra messaggio'),
             iconClass: 'icon-info',
             mime: 'all',
             permissions: OC.PERMISSION_READ,
             actionHandler: (fileName, context) => {
+                console.log('[egonextapp] azione eseguita su', fileName);
                 OC.dialogs.alert('Hai cliccato su: ' + fileName, 'Ego Next App');
             }
         });
