b62d9fbfe149670d9e67a4942abf952ac879149d
Ennio Balocchi <ennio@domoticachepassione.it>
2025-09-09 07:11:03 +0200

    menu contestuale

--- apps/egonextapp/js/main.js
+++ apps/egonextapp/js/main.js
@@ -2,22 +2,25 @@ console.log('[egonextapp] main.js caricato');
 
 (function () {
     function registerAction() {
-        if (typeof OCA === 'undefined' || typeof OCA.Files === 'undefined') {
-            console.warn('[egonextapp] OCA.Files non è disponibile, riprovo...');
+        if (typeof OCA === 'undefined' || !OCA.Files || !OCA.Files.FileActions) {
+            console.warn('[egonextapp] OCA.Files.FileActions non è disponibile, riprovo...');
             setTimeout(registerAction, 500);
             return;
         }
 
-        console.log('[egonextapp] Registro azione in OCA.Files');
+        console.log('[egonextapp] Registro azione con FileActions');
 
-        OCA.Files.fileActions.registerAction({
-            name: 'EgoNextAppAction',
+        OCA.Files.FileActions.registerAction({
+            id: 'egonextapp-action',
             displayName: t('egonextapp', 'Mostra messaggio'),
+            iconClass: 'icon-info',
             mime: 'all',
             permissions: OC.PERMISSION_READ,
-            iconClass: 'icon-info',
-            actionHandler: function (fileName, context) {
-                OC.dialogs.alert('Hai cliccato su: ' + fileName, 'Ego Next App');
+            actionHandler: (fileName, context) => {
+                OC.dialogs.alert(
+                    'Hai cliccato su: ' + fileName,
+                    'Ego Next App'
+                );
             }
         });
     }
