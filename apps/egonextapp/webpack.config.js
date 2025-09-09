a760891d396effe03bda162ff7f7818dabca07a2
Ennio Balocchi <ennio@domoticachepassione.it>
2025-09-09 19:11:11 +0200

    menu contestuale

--- apps/egonextapp/webpack.config.js
+++ apps/egonextapp/webpack.config.js
@@ -1,32 +1,36 @@
-const path = require('path');
+// webpack.config.js
+const path = require('path')
 
 module.exports = {
+  mode: 'production',
   entry: './src/main.js',
   output: {
-    filename: 'main.js',
     path: path.resolve(__dirname, 'js'),
+    filename: 'main.js',
+  },
+  target: 'web',
+  resolve: {
+    fallback: {
+      // Polyfill per moduli Node usati da @nextcloud/files
+      path: require.resolve('path-browserify'),
+      string_decoder: require.resolve('string_decoder/'),
+      // Se in futuro compaiono altri simili errori (“Module not found: stream/buffer/crypto…”)
+      // aggiungi i corrispondenti fallback qui
+      // es.: stream: require.resolve('stream-browserify'),
+      //      buffer: require.resolve('buffer/'),
+    },
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', {
-                targets: {
-                  browsers: ['extends @nextcloud/browserslist-config'],
-                },
-              }],
-            ],
-          },
-        },
-      },
+      // Se usi Babel:
+      // {
+      //   test: /\.m?js$/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: { presets: ['@babel/preset-env'] }
+      //   }
+      // }
     ],
   },
-  resolve: {
-    extensions: ['.js'],
-  },
-};
+}
