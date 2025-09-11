c07a6ab430244345e2ac0abc27dcab425e76b5cd
Ennio Balocchi <ennio@domoticachepassione.it>
2025-09-11 19:59:25 +0200

    intercetta upload

--- apps/egonextapp/lib/AppInfo/Application.php
+++ apps/egonextapp/lib/AppInfo/Application.php
@@ -7,6 +7,7 @@ use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
 use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\EventDispatcher\IEventDispatcher; 
 use OCP\Util;
 
 class Application extends App implements IBootstrap {
